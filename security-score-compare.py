952bb4d996f1402cf0a30939baad2d5f1e37297d
rozie <rozie@poczta.onet.pl>
2019-04-04 07:56:56 +0200

    initial chart generation support

--- security-score-compare.py
+++ security-score-compare.py
@@ -6,10 +6,56 @@ import logging
 import re
 import sqlite3
 
+import matplotlib
+matplotlib.use('agg')
+import matplotlib.pyplot as plt
 import requests
+
 import yaml
 
 logger = logging.getLogger(__name__)
+
+
+def get_data_from_db(dbfile, days, platform):
+    con = None
+    data = {}
+    day_query = "-{} days".format(days)
+    #print(dbfile, days, platform)
+    try:
+        conn = sqlite3.connect(dbfile)
+        cur = conn.cursor()
+        query = """SELECT  DISTINCT substr(timestamp, 1, 10), nick, score
+                FROM score WHERE platform=?
+                AND timestamp > datetime('now', ?)
+                ORDER BY timestamp;"""
+        cur.execute(query, (platform, day_query))
+        rows = cur.fetchall()
+        dates_tmp = set()
+        nicks_tmp = {}
+        for row in rows:
+            (date, nick, score) = row
+            dates_tmp.add(date)
+            if not nicks_tmp.get(nick):
+                nicks_tmp[nick] = []
+                nicks_tmp[nick].append(score)
+            else:
+                nicks_tmp[nick].append(score)
+        data['dates'] = list(dates_tmp)
+        data['scores'] = nicks_tmp
+        # pad missing with zeros
+        for nick in data.get('scores'):
+            days_of_data = int(len(data['scores'][nick]))
+            dates_count = int(len(data['dates']))
+            print(nick, days_of_data, dates_count)
+            if days_of_data < dates_count:
+                data['scores'][nick] = ([0] * (dates_count - days_of_data)) + data['scores'][nick]
+    except Exception as e:
+        logging.error("DB oparation failed %s", e)
+    finally:
+        if con:
+            con.close()
+    return data
+
 
 
 def store_to_db(dbfile, platform, nick, score):
@@ -63,11 +109,23 @@ def main():
     except Exception as e:
         logger.error("Couldn't read config file %s", e)
 
-    # rootme
     dbfile = data.get('sqlite', {}).get('db', 'score.db')
 
+    # draw a chart
     if args.plot:
-        pass
+        data = get_data_from_db(dbfile, args.time, args.platform)
+        x_axis = data.get('dates')
+        print(x_axis)
+        fig = plt.figure()
+        ax = plt.subplot(111)
+        for nick in data.get('scores'):
+            values = data['scores'][nick]
+            print(nick, values)
+            ax.plot(x_axis, values, label=nick)
+        plt.legend(loc=3)
+        fig.savefig(args.output)
+
+    # grab data from all platforms
     else:
         tmpplatforms = data['platforms']
         for platform in tmpplatforms:
@@ -107,12 +165,21 @@ def parse_arguments():
         help="Configuration file")
     parser.add_argument(
         '-p', '--plot', required=False,
-        default=False,
+        default=False, action='store_true',
         help="Plot a chart")
     parser.add_argument(
-        '-t', '--time', required=False,
-        default=28,
+        '-t', '--time', required=False, type=int,
+        default=7,
         help="How many days")
+    parser.add_argument(
+        '-o', '--output', required=False,
+        default='output.png',
+        help="Output chart file")
+    parser.add_argument(
+        '-P', '--platform', required=False,
+        default='rootme',
+        help="Platform to make chart for"
+    )
 
     args = parser.parse_args()
     return args
